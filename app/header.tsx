3cf2c393a7c35a6f2689d68184eeb943773642b7
YukiWatanabe <yutaroud@gmail.com>
2023-12-31 21:38:49 +0900

    format

--- app/header.tsx
+++ app/header.tsx
@@ -1,5 +1,5 @@
 import AppBar from "@mui/material/AppBar";
-import CssBaseline from '@mui/material/CssBaseline';
+import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
@@ -34,13 +34,13 @@ const header = () => {
             href="#app-bar-with-responsive-menu"
             sx={{
               mr: 2,
-              display: { xs: 'flex', md: 'none' },
+              display: { xs: "flex", md: "none" },
               flexGrow: 1,
-              fontFamily: 'monospace',
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
             RECT
