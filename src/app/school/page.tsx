19ca5215c9cc5a9c8b7498d00e556eb8381a22ca
snd-07 <sndnn7@gmail.com>
2024-06-18 21:39:29 +0900

    [#70]アイコン設置

--- src/app/school/page.tsx
+++ src/app/school/page.tsx
@@ -51,14 +51,14 @@ const School = () => {
             </Box>
           </Box>
         </Box>
-        <Box pt={6} pb={10}>
+        <Box pt={6} pb={10} mx={3}>
           <Typography variant="h2"><img src="../../image/school/heading_teacher.png" width="58" /></Typography>
-          <img src="../../image/school/icon.png" width="420" style={{width: '100%'}}/>
-          <Box mx={3}>
-            <Typography variant="h6" fontWeight='bold' align="left">渡辺 優樹</Typography>
-            <Typography variant="body2" mt={2} align="left">5年以上、一部上場企業にてWebエンジニアを経験。<br/>次の世代に残る何かを残したいと思い起業しました。</Typography>
-            <Typography variant="body2" mt={2} align="left">とにかくまずはプログラミングを楽しいと思ってもらえるように頑張ります！</Typography>
+          <Box mt={3}>
+            <img src="../../image/school/icon.png" width="420" style={{width: '100%'}}/>
           </Box>
+          <Typography variant="h6" fontWeight='bold' align="left">渡辺 優樹</Typography>
+          <Typography variant="body2" mt={2} align="left">5年以上、一部上場企業にてWebエンジニアを経験。<br/>次の世代に残る何かを残したいと思い起業しました。</Typography>
+          <Typography variant="body2" mt={2} align="left">とにかくまずはプログラミングを楽しいと思ってもらえるように頑張ります！</Typography>
         </Box>
         <Box pt={7} pb={7} px={3} bgcolor='#407BFF'>
           <Typography variant="h2"><img src="../../image/school/heading_cta.png" width="236" /></Typography>
