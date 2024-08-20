ff778edb60e4729f46f1410748e85796b37c3834
YukiWatanabe <yutaroud@gmail.com>
2024-08-20 22:03:17 +0900

    インデント修正

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,26 +1,32 @@
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Link from 'next/link'
+import Link from "next/link";
 import Box from "@mui/material/Box";
-import Image from 'next/image';
+import Image from "next/image";
 
 interface HeaderProps {
   switchLogo?: boolean;
 }
 
-const Header: React.FC<HeaderProps> = ({switchLogo}) => {
-
+const Header: React.FC<HeaderProps> = ({ switchLogo }) => {
   return (
     <AppBar position="fixed" sx={{ zIndex: 2000 }}>
       <Toolbar sx={{ backgroundColor: "background.paper" }}>
-          {switchLogo ? 
-          <Box style={{ maxWidth: '420px'}}><Image
-            alt='Re:ProS'
-            src='../../image/school/logo.png'
-            width={140}
-            height={38}
-        /></Box> : <Typography variant="h6" color="text.primary"><Link href="/">RE:CT</Link></Typography> }
+        {switchLogo ? (
+          <Box style={{ maxWidth: "420px" }}>
+            <Image
+              alt="Re:ProS"
+              src="../../image/school/logo.png"
+              width={140}
+              height={38}
+            />
+          </Box>
+        ) : (
+          <Typography variant="h6" color="text.primary">
+            <Link href="/">RE:CT</Link>
+          </Typography>
+        )}
       </Toolbar>
     </AppBar>
   );
