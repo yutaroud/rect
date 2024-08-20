c5e1031b6ad7b401c7ca806456cd70eba7b5401f
YukiWatanabe <yutaroud@gmail.com>
2024-08-20 22:11:36 +0900

    formatterをかけた

--- src/app/school/page.tsx
+++ src/app/school/page.tsx
@@ -1,29 +1,28 @@
 import "../globals.css";
 import Box from "@mui/material/Box";
-import KeyVisual from "./KeyVisual"
-import Curriculum from "./Curriculum"
-import Teacher from "./Teacher"
-import Contact from "./Contact"
-
+import KeyVisual from "./KeyVisual";
+import Curriculum from "./Curriculum";
+import Teacher from "./Teacher";
+import Contact from "./Contact";
 
 const School = () => {
   return (
     <main>
       <Box
-      component="section"
-      mt={8}
-      sx={{
+        component="section"
+        mt={8}
+        sx={{
           textAlign: "center",
-          mx: 'auto',
-          border: 'solid 1px #EEEEEE',
-          maxWidth: '420px',
-          width: '100%'
-      }}
+          mx: "auto",
+          border: "solid 1px #EEEEEE",
+          maxWidth: "420px",
+          width: "100%",
+        }}
       >
-        <KeyVisual/>
-        <Curriculum/>
-        <Teacher/>
-        <Contact/>
+        <KeyVisual />
+        <Curriculum />
+        <Teacher />
+        <Contact />
       </Box>
     </main>
   );
