36c7facdcae348fff7f3f9c91cc18f41a2855f90
snd-07 <sndnn7@gmail.com>
2024-08-20 22:00:17 +0900

    ロゴ入れた

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -2,6 +2,8 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Link from 'next/link'
+import Box from "@mui/material/Box";
+import Image from 'next/image';
 
 interface HeaderProps {
   switchLogo?: boolean;
@@ -12,9 +14,13 @@ const Header: React.FC<HeaderProps> = ({switchLogo}) => {
   return (
     <AppBar position="fixed" sx={{ zIndex: 2000 }}>
       <Toolbar sx={{ backgroundColor: "background.paper" }}>
-        <Typography variant="h6" color="text.primary">
-          {switchLogo ? <Link href="/">Re:ProS</Link> : <Link href="/">RE:CT</Link> }
-        </Typography>
+          {switchLogo ? 
+          <Box style={{ maxWidth: '420px'}}><Image
+            alt='Re:ProS'
+            src='../../image/school/logo.png'
+            width={140}
+            height={38}
+        /></Box> : <Typography variant="h6" color="text.primary"><Link href="/">RE:CT</Link></Typography> }
       </Toolbar>
     </AppBar>
   );
