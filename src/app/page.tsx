cef4a20d76229f0486f6e7b5f8e3b03f68926d61
YukiWatanabe <yutaroud@gmail.com>
2024-10-15 12:40:35 +0900

    canonical追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,15 @@ import Business from "../modules/Business";
 import Recruitment from "@/modules/Recuritment";
 import Contact from "@/modules/Contact";
 
+export const metadata = {
+  title: "株式会社レクト",
+  description: "開発業務の請負やプログラミング教室の運営を行っております。",
+  robots: "index,follow",
+  alternates: {
+    canonical: "https://re-ct.co.jp"
+  }
+};
+
 export default function Home() {
   return (
     <main style={{paddingBottom: "80px"}}>
