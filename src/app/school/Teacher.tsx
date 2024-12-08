75d914aacbf64949be08298115baf843bab1bdef
snd-07 <sndnn7@gmail.com>
2024-12-08 20:35:01 +0900

    [#131]fix:講師コンテンツの修正

--- src/app/school/Teacher.tsx
+++ src/app/school/Teacher.tsx
@@ -15,40 +15,75 @@ const sxStyles = {
       pl: 0,
       py: 0,
     },
+  },
+  heading: {
+    marginInline: "24px",
+    '& > img': {
+      width: "81px",
+      height: "auto",
+    },
+    "@media screen and (max-width:450px)": {
+      '& > img': {
+        width: "58px",
+      }
+    }
+  },
+  wrap: {
+    mt: 9,
+    display: "flex",
+    width: "800px",
+    mx: "auto",
+    alignItems: "flex-start",
+    columnGap: "32px",
+    flexDirection: "row-reverse",
+    "@media screen and (max-width:450px)": {
+      width: "100%",
+      mt: 3,
+      flexDirection: "column",
+    },
+    '& > img': {
+      width: "327px",
+      height: "auto",
+    }
+  },
+  text: {
+    width: "100%"
   }
 }
 
 const Teacher = () => {
   return (
     <Box pt={6} pb={10} mx={3} id="school_teacher">
-      <Typography variant="h2">
-        <img src={headingTeacherImg.src} alt="講師" width="58" />
+      <Typography variant="h2" sx={sxStyles.heading}>
+        <img src={headingTeacherImg.src} alt="講師" width="127" height="59" />
       </Typography>
-      <Box mt={3}>
-        <img src={iconImg.src} width="420" style={{ width: "100%" }} />
+      <Box sx={sxStyles.wrap}>
+        <img src={iconImg.src} width="654" height="568" />
+        <Box sx={sxStyles.text}>
+          <Typography variant="h6" fontWeight="bold" align="left">
+            渡辺 優樹
+          </Typography>
+          <Typography variant="body2" mt={2} align="left">
+            プライム上場のエイチームグループにて
+            <br />
+            5年間Webエンジニアを経験し、5人のメンバーのマネジメントも経験しました。
+          </Typography>
+          <Typography variant="body2" mt={2} align="left">
+            プログラミングを通して、次の世代に残る何かを残したいと思い起業しました！
+          </Typography>
+          <Typography variant="body2" mt={2} align="left">
+            とにかくまずはプログラミングを楽しいと思ってもらえるように頑張ります！
+          </Typography>
+          <Typography variant="subtitle2" fontWeight={"bold"} mt={3} align="left">
+            経歴・資格について
+          </Typography>
+          <List sx={sxStyles.list}>
+            <ListItem>名古屋大学大学院情報学研究科 修了</ListItem>
+            <ListItem>高等学校教諭一種免許状「情報」 免許保有</ListItem>
+            <ListItem>基本情報技術者 資格保有</ListItem>
+          </List>
+        </Box>
       </Box>
-      <Typography variant="h6" fontWeight="bold" align="left">
-        渡辺 優樹
-      </Typography>
-      <Typography variant="body2" mt={2} align="left">
-        プライム上場のエイチームグループにて
-        <br />
-        5年間Webエンジニアを経験し、5人のメンバーのマネジメントも経験しました。
-      </Typography>
-      <Typography variant="body2" mt={2} align="left">
-        プログラミングを通して、次の世代に残る何かを残したいと思い起業しました！
-      </Typography>
-      <Typography variant="body2" mt={2} align="left">
-        とにかくまずはプログラミングを楽しいと思ってもらえるように頑張ります！
-      </Typography>
-      <Typography variant="subtitle2" fontWeight={"bold"} mt={3} align="left">
-        経歴・資格について
-      </Typography>
-      <List sx={sxStyles.list}>
-        <ListItem>名古屋大学大学院情報学研究科 修了</ListItem>
-        <ListItem>高等学校教諭一種免許状「情報」 免許保有</ListItem>
-        <ListItem>基本情報技術者 資格保有</ListItem>
-      </List>
     </Box>
   );
 };
