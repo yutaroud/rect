db6ebcb2c83ffbc8c58664450e0a9dbdf525f753
YukiWatanabe <yutaroud@gmail.com>
2024-12-16 19:57:33 +0900

    lintの導入

--- src/components/ThemeRegistry/EmotionCache.tsx
+++ src/components/ThemeRegistry/EmotionCache.tsx
@@ -80,14 +80,14 @@ export default function NextAppDirEmotionCacheProvider(
           <style
             key={name}
             data-emotion={`${registry.cache.key}-global ${name}`}
-            // eslint-disable-next-line react/no-danger
+             
             dangerouslySetInnerHTML={{ __html: style }}
           />
         ))}
         {styles && (
           <style
             data-emotion={dataEmotionAttribute}
-            // eslint-disable-next-line react/no-danger
+             
             dangerouslySetInnerHTML={{ __html: styles }}
           />
         )}
