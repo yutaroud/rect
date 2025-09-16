8fbeb095298e5ce7d18435136a845dc07dfb77ef
rect20250725 <rect20250725@rect20250725noMacBook-Air.local>
2025-09-16 16:13:16 +0900

    heading2の作成

---
+++ src/app/demand/components/Heading2.tsx
@@ -0,0 +1,45 @@
+import { Box, Typography } from '@mui/material';
+
+interface Heading2Props {
+  subText: string;
+  mainText: string;
+  position: 'center' | 'right', 
+}
+
+export const Heading2: React.FC<Heading2Props> = ({ subText, mainText, position="center" }) => {
+  return (
+    <Box sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: position === 'right' ? 'flex-start' : 'center',
+      textAlign: position === 'right' ? 'left' : 'center',
+      gap: '4px'
+    }}>
+      <Typography
+        variant="body2"
+        component="span"
+        sx={{
+          color: 'rgba(0, 41, 138, 0.4)',
+          fontWeight: 'bold',
+          letterSpacing: '0.07em',
+          fontSize: '18px'
+        }}
+      >
+        {subText}
+      </Typography>
+      <Typography
+      variant="h2"
+      component="h2"
+      sx={{
+        fontWeight: 'bold',
+        letterSpacing: '0.05em',
+        fontSize: '36px'
+      }}>
+        {mainText}
+      </Typography>
+    </Box>
+  )
+}
+
+export default Heading2;
