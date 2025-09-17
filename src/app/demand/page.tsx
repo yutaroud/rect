9fed9d8f2648c5fe63cd57566366d5dbd8d50db6
rect20250725 <rect20250725@rect20250725noMacBook-Air.local>
2025-09-17 14:40:40 +0900

    パンクズ

--- src/app/demand/page.tsx
+++ src/app/demand/page.tsx
@@ -1,4 +1,5 @@
 import '../globals.css';
+import BreadcrumbsNavigation from '../BreadcrumbsNavigation';
 import KeyVisual from './KeyVisual';
 import Service from './Service';
 import Case from './Case';
@@ -7,6 +8,11 @@ import Flow from './Flow';
 import Faq from './Faq';
 import Contact from './Contact';
 import Company from './Company';
+import { demandTopTitle } from '@/lib/const/BreadCrumbTitle';
+
+export const metadata = {
+  title: '開発等の相談 | 株式会社レクト',
+};
 
 const Demand = () => {
   return (
@@ -19,6 +25,7 @@ const Demand = () => {
       <Faq />
       <Contact />
       <Company />
+      <BreadcrumbsNavigation titles={demandTopTitle} />
     </main>
   );
 };
--- src/lib/const/BreadCrumbTitle.tsx
+++ src/lib/const/BreadCrumbTitle.tsx
@@ -1,1 +1,2 @@
 export const schoolTopTitle = ['プログラミング教室 Re:ProS'];
+export const demandTopTitle = ['開発等の相談'];
