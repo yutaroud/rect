e28249b015870515ce5e4d01dd6415a02b26b4c8
rect20250725 <rect20250725@rect20250725noMacBook-Air.local>
2025-09-17 10:52:05 +0900

    開発事例

--- src/app/demand/Case.tsx
+++ src/app/demand/Case.tsx
@@ -1,7 +1,135 @@
+import Heading2 from './components/Heading2'
+import { Box,Typography } from '@mui/material';
+
+const sxStyles = {
+  headingText: {
+    fontSize: '14px',
+    letterSpacing: '0.05em',
+    lineHeight: '1.7',
+    mt: 2,
+    width: '100%'
+  },
+  title: {
+    fontSize: '20px',
+    fontWeight: 'bold',
+    letterSpacing: '0.05em',
+    lineHeight: '1.6',
+  },
+  text: {
+    fontSize: '14px',
+    letterSpacing: '0.05em',
+    lineHeight: '1.7',
+  },
+  list: {
+    listStyle: 'none',
+    width: '100%',
+  },
+  listItem: {
+    background: '#fff',
+    borderRadius: '8px',
+    width: '100%',
+    px: 3,
+    py: 4,
+    boxShadow: '0px 1px 5px rgba(34, 106, 170, 0.2)',
+    '& + &': {
+      mt: 3,
+    }
+  },
+  listItemText: {
+    mt: 2,
+    listStylePosition: 'inside',
+    listStyleType: 'disc',
+  },
+  tech: {
+    mt: 1,
+  },
+  techWrap: {
+    display: 'flex',
+    gap: 2,
+    '& > dt' : {
+      color: '#545454',
+      fontSize: '12px',
+      letterSpacing: '0.05em',
+      lineHeight: '1.7',
+      minWidth: '66px',
+      maxWidth: '66px',
+    },
+    '& > dd' : {
+      color: '#545454',
+      fontSize: '12px',
+      letterSpacing: '0.05em',
+      lineHeight: '1.7',
+    },
+    '& + &': {
+      mt:0.5,
+    }
+  },
+}
 
 const Case = () => {
   return (
-    <p></p>
+    <Box
+      sx={{
+        pt: 7,
+        pb: 12,
+      }}
+    >
+      <Box
+        sx={{
+          maxWidth: '900px',
+          width: '100%',
+          mx: 'auto',
+          display: 'flex',
+          alignItems: 'flex-start',
+          gap : 6,
+        }}
+      >
+        <Box sx={{minWidth: '22%'}}>
+          <Heading2 subText="case" mainText="開発事例" position='right'/>
+          <Typography component="p" sx={sxStyles.headingText}>弊社エンジニアが<br/>実際に対応した業務の<br/>一部をご紹介いたします。</Typography>
+        </Box>
+        <Box component="ul" sx={sxStyles.list}>
+          <Box component="li" sx={sxStyles.listItem}>
+            <Typography component="h3" sx={sxStyles.title}>コーポレートサイトの開発</Typography>
+            <Box component="ul" sx={sxStyles.listItemText}>
+              <Typography component="li" sx={sxStyles.text}>UI・UXから一貫したシステム開発</Typography>
+              <Typography component="li" sx={sxStyles.text}>要件に合わせた独自プラグインの開発</Typography>
+              <Typography component="li" sx={sxStyles.text}>担当者が簡単に更新できるカスタマイズCMSの実装</Typography>
+            </Box>
+            <Box component="dl" sx={sxStyles.tech}>
+              <Box sx={sxStyles.techWrap}>
+                <Typography component="dt">技術・言語</Typography>
+                <Typography component="dd">WordPress</Typography>
+              </Box>
+              <Box sx={sxStyles.techWrap}>
+                <Typography component="dt">開発期間</Typography>
+                <Typography component="dd">1ヶ月</Typography>
+              </Box>
+            </Box>
+          </Box>
+          <Box component="li" sx={sxStyles.listItem}>
+            <Typography component="h3" sx={sxStyles.title}>レガシーシステムのモダン化</Typography>
+            <Box component="ul" sx={sxStyles.listItemText}>
+              <Typography component="li" sx={sxStyles.text}>Rails4からRails6(当時の最新版)へのバージョンアップ対応</Typography>
+              <Typography component="li" sx={sxStyles.text}>RSpecを用いたテストの整備</Typography>
+              <Typography component="li" sx={sxStyles.text}>RuboCopの導入/CIでの自動判定</Typography>
+              <Typography component="li" sx={sxStyles.text}>開発環境の構築時間を短縮するためのコンテナ化・EC2からFargateへの移管</Typography>
+              <Typography component="li" sx={sxStyles.text}>Terraformでの本番環境のコード化</Typography>
+            </Box>
+            <Box component="dl" sx={sxStyles.tech}>
+              <Box sx={sxStyles.techWrap}>
+                <Typography component="dt">技術・言語</Typography>
+                <Typography component="dd">Ruby, Ruby on Rails, AWS Fargate, Terraform</Typography>
+              </Box>
+              <Box sx={sxStyles.techWrap}>
+                <Typography component="dt">開発期間</Typography>
+                <Typography component="dd">2ヶ月</Typography>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   )
 }
 export default Case;
